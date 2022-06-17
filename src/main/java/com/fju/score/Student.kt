37d15b04af1b8ada4d029c8493efbc3aa28bba29
YunQi <mei970073@gmail.com>
2022-06-17 15:07:06 +0800

    Kotlin student

---
+++ src/main/java/com/fju/score/Student.kt
@@ -0,0 +1,19 @@
+package com.fju.com.fju.score
+
+class Student(var name:String, var english:Int, var math:Int) {
+    init {
+//        println("Testing")
+    }
+    fun print() {
+        println("$name\t$english\t$math\t${getAverage()}")
+    }
+    fun getAverage() : Int {
+        return (english+math)/2
+    }
+}
+
+fun main() {
+    val stu = Student("Jack", 77, 51)
+    stu.print()
+//    println(stu.name +"\t"+ stu.english +"\t"+ stu.math)
+}
